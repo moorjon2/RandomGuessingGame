93f5dc707238a414af42825eb8f073bf2c58305c
Michael Fernandez <mfernandezm85@gmail.com>
2023-02-13 16:29:09 -0500

    Added viewbinding a toggle switch and a button

--- app/src/main/java/com/example/workingwithimages/MainActivity.kt
+++ app/src/main/java/com/example/workingwithimages/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.workingwithimages
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.workingwithimages.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var fruits = mutableListOf(R.drawable.apple, R.drawable.grape, R.drawable.orange)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        fruits.add(R.drawable.ic_orange_vector)
+        binding.btnRandom.setOnClickListener {
+            val r = (fruits).shuffled().first()
+            binding.imageViewFruit.setImageResource(r)
+            //Snackbar.make(it, "random number is $r", Snackbar.LENGTH_LONG).show()
+        }
+
+        binding.imageViewFruit.setImageResource(R.drawable.ic_orange_vector)
+        binding.switchFruit.setOnCheckedChangeListener { _, isChecked ->
+            toggleImage(isChecked)
+        }
+    }
+
+    private fun toggleImage(isChecked: Boolean) {
+        if (isChecked) {
+            binding.imageViewFruit.setImageResource(R.drawable.grape)
+            binding.switchFruit.text = getString(R.string.switch_apple)
+        } else {
+            binding.imageViewFruit.setImageResource(R.drawable.apple)
+            binding.switchFruit.text = getString(R.string.switch_grapes)
+        }
     }
 }
