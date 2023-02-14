16d25c698f8a997995ea5b88d7d81a42874092dd
Michael Fernandez <mfernandezm85@gmail.com>
2023-02-13 23:24:52 -0500

    Snackbars for the buttons, while switch is not checked

--- app/src/main/java/com/example/workingwithimages/MainActivity.kt
+++ app/src/main/java/com/example/workingwithimages/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.workingwithimages
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Switch
 import com.example.workingwithimages.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 
@@ -15,26 +18,58 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        fruits.add(R.drawable.ic_orange_vector)
+        binding.btnApple.setOnClickListener {
+            snackStart(it)
+        }
+        binding.btnGrape.setOnClickListener {
+            snackStart(it)
+        }
         binding.btnRandom.setOnClickListener {
-            val r = (fruits).shuffled().first()
-            binding.imageViewFruit.setImageResource(r)
-            //Snackbar.make(it, "random number is $r", Snackbar.LENGTH_LONG).show()
+            snackStart(it)
         }
 
-        binding.imageViewFruit.setImageResource(R.drawable.ic_orange_vector)
         binding.switchFruit.setOnCheckedChangeListener { _, isChecked ->
             toggleImage(isChecked)
         }
     }
 
+    private fun snackStart(it: View) {
+        Snackbar.make(it, "Switch to Start game", Snackbar.LENGTH_LONG).show()
+    }
+
     private fun toggleImage(isChecked: Boolean) {
+        var r: Int = 0
+
         if (isChecked) {
-            binding.imageViewFruit.setImageResource(R.drawable.grape)
-            binding.switchFruit.text = getString(R.string.switch_apple)
+            binding.switchFruit.text = getString(R.string.stopGame)
+
+            binding.btnRandom.setOnClickListener {
+                r = (fruits).shuffled().first()
+                binding.IVRandom.setImageResource(r)
+            }
+            binding.btnGrape.setOnClickListener {
+                binding.IVMyFruit.setImageResource(R.drawable.grape)
+            }
+            binding.btnApple.setOnClickListener {
+                binding.IVMyFruit.setImageResource(R.drawable.apple)
+            }
         } else {
-            binding.imageViewFruit.setImageResource(R.drawable.apple)
-            binding.switchFruit.text = getString(R.string.switch_grapes)
+            binding.switchFruit.text = getString(R.string.startGame)
+        }
+            //checkImages(r)
+    }
+
+    // TODO: need to implement winning condition
+    // Unsure as to how to implement this logic
+    // Need to check if the random image and the image from one of the two
+    // buttons (btnGrape/btnApple) are the same. While the switch is checked
+    // If they are the same show a Snackbar with the message "Congratulations! You win"
+    // else if they are not the same "Sorry, you loose"
+    private fun checkImages(image: Int) {
+        return if (fruits.contains(image)) {
+            Snackbar.make(binding.root, "Congratulations", Snackbar.LENGTH_LONG).show()
+        } else {
+            Snackbar.make(binding.root, "Sorry you lose", Snackbar.LENGTH_LONG).show()
         }
     }
 }
